e8566eb892e33658e3caa6a3dbd5113e94accf90
Mayur Tank <mayurtank@Mayurs-MacBook-Air.local>
2025-11-16 11:09:50 +0530

    Updated viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
-    hmr: {
-      overlay: true,
-      clientPort: 5173,
+    proxy: {
+      '/api': 'https://laxmipowertech-backend.onrender.com', // 👈 proxies /api to backend
     },
-    // Remove proxy - we're using axios baseURL instead
   },
   resolve: {
     alias: {
